615720be771a0bbe144c500dd8ce263e660c7218
Kevin Gao <kevinjamesgao@gmail.com>
2023-06-22 12:35:41 -0700

    updated button logo

--- src/Login.js
+++ src/Login.js
@@ -15,7 +15,13 @@ const Login = () => {
         provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
         fullLabel: "Sign in with Email",
       },
-      { provider: "oidc.descope", fullLabel: "Sign in With Descope Passkeys" },
+      {
+        provider: "oidc.descope",
+        fullLabel: "Sign in with Descope Passkeys",
+        buttonColor: "#000000",
+        iconUrl:
+          "https://images.ctfassets.net/xqb1f63q68s1/7D1PYGYvVgRNOBeiA6USQM/68b572056b5d38a769c71b0fba63b4e5/Descope_RGB_Icon-ForDarkBackground.svg",
+      },
     ],
     callbacks: {
       signInSuccessWithAuthResult: () => {
